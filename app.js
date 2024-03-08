11d1a24a7c0f230622fc6a1a7ae7635811dbe1b5
Habib-0007 <adebayohabib7@gmail.com>
2024-03-08 11:03:09 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+const app = express();
+const PORT = 4000;
+
+app.set("views", "./view");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	var blogs = [
+		{
+			name: "Lorem ipsum dolor",
+			snippet: "Lorem ipsum dolor sit amet, consectetur"
+		},
+		{
+			name: "sit amet consectetur.",
+			snippet: "adipisicing elit. Veritatis soluta voluptatibus eum."
+		},
+		{
+			name: "Temporibus laudantium excepturi inventore!",
+			snippet:
+				"Pariatur natus similique necessitatibus alias ratione blanditiis eaque."
+		}
+	];
+	res.render("index", { title: "Homepage", blogs });
+});
+
+app.get("/about", (req, res) => {
+	res.render("about", { title: "About Blog" });
+});
+
+app.get("/home", (req, res) => {
+	res.redirect("/");
+});
+
+app.get("/blogs/create", (req, res) => {
+	res.render("create", { title: "Create a new blog" });
+});
+
+app.use((req, res) => {
+	res.render("404");
+});
+
+app.listen(PORT, () => {
+	console.log(`Your server is running at ${PORT}`);
+});
